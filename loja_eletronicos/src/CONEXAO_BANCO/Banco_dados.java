8527c564b7c11c66610545438c4cbc95e84be949
LeandroPS121 <dleandropaulo@gmail.com>
2024-07-15 09:05:54 -0300

    q

---
+++ loja_eletronicos/src/CONEXAO_BANCO/Banco_dados.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CONEXAO_BANCO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author sil9jvl
+ */
+public class Banco_dados {
+    public Connection conexao = null;
+    private final String driver = "com.mysql.cj.jdbc.Driver";
+    private final String nomedobanco = "loja_eletronicos";
+    private final String local = "jdbc:mysql://localhost:3306/" +nomedobanco;
+    private final String login = "root";
+    private final String senha = "root";
+    
+    public boolean getConnection() {
+        try{
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(local,login,senha);
+            System.out.println("banco de dados conectado");
+            return true;
+        } catch(ClassNotFoundException erro){
+            System.out.println("Drive não encontrado" +erro.toString());
+            return false;
+        } catch(SQLException erro){
+            System.out.println("Falha na conexão"+erro.toString());
+            return false;
+        }
+    }
+}
